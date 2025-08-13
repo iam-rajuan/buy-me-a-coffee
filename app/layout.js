bd22fd691f8008e7fe4e1092e9810062b3e7a730
Md Rajuan Hossen <rajuan.official@gmail.com>
2025-08-13 18:27:04 +0600

    save-previous-update

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,12 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -22,13 +22,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className="bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] flex flex-col min-h-screen">
-        <Navbar/>
-        <div className="flex-grow">
-        {children}
-        </div>
-        <Footer/>
+      <body className="bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] flex flex-col min-h-screen">
+        <SessionWrapper>
+          <Navbar />
+          <div className="flex-grow">{children}</div>
+          <Footer />
+        </SessionWrapper>
       </body>
     </html>
   );
